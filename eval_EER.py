ae4359ebc3d34fbb728d943b00604e65f0859765
aesalman <aesalman14@gmail.com>
2023-11-16 09:54:35 -0500

    Added eval_EER.

---
+++ eval_EER.py
@@ -0,0 +1,79 @@
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compute_det_curve(target_scores, nontarget_scores):
+
+    n_scores = target_scores.size + nontarget_scores.size
+    all_scores = np.concatenate((target_scores, nontarget_scores))
+    labels = np.concatenate((np.ones(target_scores.size), np.zeros(nontarget_scores.size)))
+
+    # Sort labels based on scores
+    indices = np.argsort(all_scores, kind='mergesort')
+    labels = labels[indices]
+
+    # Compute false rejection and false acceptance rates
+    tar_trial_sums = np.cumsum(labels)
+    nontarget_trial_sums = nontarget_scores.size - (np.arange(1, n_scores + 1) - tar_trial_sums)
+
+    frr = np.concatenate((np.atleast_1d(0), tar_trial_sums / target_scores.size))  # false rejection rates
+    far = np.concatenate((np.atleast_1d(1), nontarget_trial_sums / nontarget_scores.size))  # false acceptance rates
+    thresholds = np.concatenate((np.atleast_1d(all_scores[indices[0]] - 0.001), all_scores[indices]))  # Thresholds are the sorted scores
+
+    return frr, far, thresholds
+
+def compute_eer(target_scores, nontarget_scores):
+    """ Returns equal error rate (EER) and the corresponding threshold. """
+    frr, far, thresholds = compute_det_curve(target_scores, nontarget_scores)
+    abs_diffs = np.abs(frr - far)
+    min_index = np.argmin(abs_diffs)
+    eer = np.mean((frr[min_index], far[min_index]))
+    return eer, thresholds[min_index]
+
+args = sys.argv
+
+if 'dev'==args[1]:
+    
+    ASV_SCOREFILE = 'tDCF_python/ASV_scores/ASVspoof2019.LA.asv.dev.gi.trl.scores.txt'
+    CM_SCOREFILE = 'dev_CM_add_scores_file.txt'
+    
+elif 'Eval'==args[1]:
+    ASV_SCOREFILE = 'C:/Users/400743/Documents/Research - Anti_spoofing/Rob-Eval-Antispoofing/txtfiles/ASVspoof2019.LA.cm.eval.trl.txt'
+    CM_SCOREFILE = 'Scores.txt'
+     
+# Replace CM scores with your own scores or provide score file as the first argument.
+cm_scores_file =  CM_SCOREFILE
+# Replace ASV scores with organizers' scores or provide score file as the second argument.
+asv_score_file =  ASV_SCOREFILE
+
+# Fix tandem detection cost function (t-DCF) parameters
+Pspoof = 0.05
+cost_model = {
+    'Pspoof': Pspoof,  # Prior probability of a spoofing attack
+    'Ptar': (1 - Pspoof) * 0.99,  # Prior probability of target speaker
+    'Pnon': (1 - Pspoof) * 0.01,  # Prior probability of nontarget speaker
+    'Cmiss_asv': 1,  # Cost of ASV system falsely rejecting target speaker
+    'Cfa_asv': 10,  # Cost of ASV system falsely accepting nontarget speaker
+    'Cmiss_cm': 1,  # Cost of CM system falsely rejecting target speaker
+    'Cfa_cm': 10,  # Cost of CM system falsely accepting spoof
+}
+
+
+# Load CM scores
+cm_data = np.genfromtxt(cm_scores_file, dtype=str)
+cm_utt_id = cm_data[:, 0]
+cm_sources = cm_data[:, 1]
+cm_keys = cm_data[:, 2]
+cm_scores = cm_data[:, 3].astype(float)
+
+
+# Extract bona fide (real bonafide) and spoof scores from the CM scores
+bona_cm = cm_scores[cm_keys == 'bonafide']
+spoof_cm = cm_scores[cm_keys == 'spoof']
+
+# EERs of the standalone systems and fix ASV operating point to EER threshold
+eer_cm = compute_eer(bona_cm, spoof_cm)[0]
+
+print('\nCM SYSTEM')
+print('   EER            = {:8.9f} % (Equal error rate for countermeasure)'.format(eer_cm * 100))
